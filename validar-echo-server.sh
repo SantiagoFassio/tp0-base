92455151e4e8521b1748b40bb8a39f450e71fd29
santiagoFassio <fassiosantiago@gmail.com>
2026-03-18 14:26:00 -0300

    ej3: solucion: creacion del script para el echo server test

---
+++ validar-echo-server.sh
@@ -0,0 +1,11 @@
+MESSAGE="ping test ej3"
+
+RESPONSE=$(docker run --rm --network testing_net busybox sh -c "
+    echo '$MESSAGE' | nc server 12345
+" 2>/dev/null | tr -d '\r')
+
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
