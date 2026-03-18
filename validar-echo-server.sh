04cbcb7d687553b5ee0bf2eda74b3f39ba6e5376
santiagoFassio <fassiosantiago@gmail.com>
2026-03-18 15:21:47 -0300

    fix: arreglado del ej 3 ya que comparacion script bash hace == en vez de =

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,10 @@
-MESSAGE="ping test ej3"
+MESSAGE="ej3"
 
 RESPONSE=$(docker run --rm --network testing_net busybox sh -c "
     echo '$MESSAGE' | nc server 12345
 " 2>/dev/null | tr -d '\r')
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
