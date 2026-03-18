8b7b46f2f9a16acd94b295dd264f90023a1de980
santiagoFassio <fassiosantiago@gmail.com>
2026-03-18 16:35:50 -0300

    fix: 1. cambiar validar-echo-server.sh para ahora usar printf 2. cambiado main.go para tomar /config.yaml y no ./cinfig.yaml

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 MESSAGE="ej3"
 
-RESPONSE=$(docker run --rm --network testing_net busybox sh -c "
-    echo '$MESSAGE' | nc server 12345
-" 2>/dev/null | tr -d '\r')
+RESPONSE=$(docker run --rm --network tp0_testing_net busybox sh -c "
+    printf '%s\n' '$MESSAGE' | nc -w 2 server 12345
+" 2>/dev/null | tr -d '\r\n')
 
 if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
